e276e68e6b1169eef5980777a8d1d9fd22bd736f
rmunozca <147057861+rmunozca@users.noreply.github.com>
2025-06-07 23:53:29 -0400

    Delete cellSegmentation directory

--- cellSegmentation/networkTraining/buildDetectionModel.m
+++
@@ -1,4 +0,0 @@
-function lgraph = buildYOLOModel(inputSize, numClasses)
-%BUILDYOLOMODEL Constructs a YOLOv2 object detector with ResNet-50
-lgraph = yolov2Layers(inputSize, numClasses, 'resnet50');
-end
